b62f70a5246938ff95d12f1471ca62ba6a7b2582
mixmastert <torahlight@gmail.com>
2017-06-06 00:38:22 -0500

    closest pair problem mostly working, but stack overflow occurs if more than three points along xMid

--- closest_pair.js
+++ closest_pair.js
@@ -4,7 +4,7 @@ const simpleSet = [
   [1,5],
   [3,3],
 ]
-/*
+/* closest = 4,1 - 5,2
 5X
 4
 3    X
@@ -25,7 +25,7 @@ const biggerSet = [
   [9,1],
   [6,2],
 ]
-/*
+/* closest = 4,1 - 5,2
 9    X
 8      X     X
 7
@@ -38,36 +38,104 @@ const biggerSet = [
 X1 2 3 4 5 6 7 8 9
 */
 
+const hardSet = [
+  [0,0],
+  [5,5],
+  [8,5],
+  [5,3],
+]
+/*
+/* closest = 4,1 - 5,2
+9
+8
+7
+6
+5        X     X
+4
+3        X
+2
+1
+X1 2 3 4 5 6 7 8 9
+*/
 const closestPair = (ptsArr) => {
   ptsArr.sort((p1, p2) => p1[0] - p2[0]);
   const closest = orderedSearch(ptsArr);
   return closest;
 }
 
+const getDist = (p1, p2) => {
+  return Math.sqrt((p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]));
+}
+
 console.log(closestPair(biggerSet));
 
-const orderedSearch = (ordPtsArr) => {
+function orderedSearch(ordPtsArr){
   if (ordPtsArr.length) {
+    if (ordPtsArr.length < 2) {
+      return null;
+    }
     if (ordPtsArr.length === 2) {
-      return Math.sqrt(ordPtsArr[0] * ordPtsArr[0] + ordPtsArr[1] * ordPtsArr[1]);
+      return getDist(ordPtsArr[0], ordPtsArr[1]);
     }
-    const xMid = (ordPts[ordPts.length - 1][0] + ordPts[0][0]) / 2;
-    const firstClosest = orderedSearch(ordPtsArr.filter((pt) => pt[0] < xMid)));
-    const secondClosest = orderedSearch(ordPtsArr.filter((pt) => pt[0] >= xMid));
+    const xMid = (ordPtsArr[ordPtsArr.length - 1][0] + ordPtsArr[0][0]) / 2;
+    console.log(xMid, ordPtsArr);
+    const leftClosest = orderedSearch(ordPtsArr.filter((pt) => pt[0] < xMid));
+    const rightClosest = orderedSearch(ordPtsArr.filter((pt) => pt[0] >= xMid));
 
-    const shorter = math.min(firstClosest, secondClosest);
+    let minDist = leftClosest && rightClosest ?
+                    Math.min(leftClosest, rightClosest) :
+                    leftClosest || rightClosest;
 
-    const leftStripPoints = ordPtsArr.filter((pt) => pt[0] < xMid && xMid - pt[0] < shorter);
-    const rightStripPoints = ordPtsArr.filter((pt) => pt[0] > xMid && pt[0] - xMid < shorter);
+    const leftStripPoints = ordPtsArr.filter((pt) => pt[0] <= xMid && xMid - pt[0] < minDist);
+    const rightStripPoints = ordPtsArr.filter((pt) => pt[0] > xMid && pt[0] - xMid < minDist);
 
-    const ySortedLeftStripPoints = leftStripPoints.slice();
-    ySortedLeftStripPoints.sort((p1, p2) => p1[1] - p2[1]);
+    if (leftStripPoints.length && rightStripPoints.length) {
 
-    leftStripPoints.forEach((pt) => {
-      // find closest y above and closest y below
-    })
-    if (ordPtsArr.length === 3) {
+      if (leftStripPoints.length === 1 && rightStripPoints.length === 1) {
+        const stripDist = getDist(leftStripPoints[0], rightStripPoints[0]);
+        return Math.min(stripDist, minDist);
+      } else {
 
+        const ySortedLeftStripPoints = leftStripPoints.slice();
+        ySortedLeftStripPoints.sort((p1, p2) => p1[1] - p2[1]);
+
+        const ySortedRightStripPoints = rightStripPoints.slice();
+        ySortedRightStripPoints.sort((p1, p2) => p1[1] - p2[1]);
+
+        let rightPointer = 0;
+        leftStripPoints.forEach((pt, leftPointer) => {
+          const upperBound = Math.min(pt[1] + minDist, ySortedRightStripPoints[ySortedRightStripPoints.length - 1][1]);
+          // this while loop will have at most 4 cycles, because if two points
+          // exist within the minDist bounded box on either side, they would have
+          /// been evaluated as the value for minDist...
+          while (ySortedRightStripPoints[rightPointer] &&
+            ySortedRightStripPoints[rightPointer][1] - pt[1] < minDist) {
+              const currentDist = getDist(pt, ySortedRightStripPoints[rightPointer]);
+              if (currentDist < minDist) {
+                minDist = currentDist;
+              }
+              rightPointer++;
+            }
+            if (leftPointer < ySortedLeftStripPoints.length - 1) {
+              const nextLeft = ySortedLeftStripPoints[leftPointer + 1];
+              while (ySortedRightStripPoints[rightPointer] &&
+                      ySortedRightStripPoints[rightPointer][1] > nextLeft[1]) {
+                rightPointer--;
+              }
+            }
+          })
+
+
+
+
+      }
     }
+
+    // if (ordPtsArr.length === 3) {
+    //
+    // }
+  return minDist;
   }
 }
+
+console.log(closestPair(hardSet));
